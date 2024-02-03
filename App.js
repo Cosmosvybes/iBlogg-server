111579c5d30b250d15f1170d4b0c1b8cf4c4e495
Cosmos <Alfredchrisayo@gmail.com>
2024-02-03 11:00:57 +0000

    add .gitignore

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ const { uploadImage } = require("./Middleware/upload");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
+
 app.post("/api/create", uploadImage(), createPost);
+
+
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
