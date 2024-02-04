f65d971a28523f31476c1f69c3b043bcbb49c891
Cosmos <Alfredchrisayo@gmail.com>
2024-02-04 00:24:12 +0000

    add

--- App.js
+++ App.js
@@ -9,10 +9,13 @@ const { createPost, getPosts } = require("./Routes/Api");
 const { uploadImage } = require("./Middleware/upload");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join("dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.post("/api/create", uploadImage(), createPost);
 app.get("/api/posts", getPosts);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname, "dist", "index.html");
+});
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
