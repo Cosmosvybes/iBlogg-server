418e25aa9809dc538c96ad7399b4c9803594e333
Cosmos <Alfredchrisayo@gmail.com>
2024-03-07 12:53:49 +0000

    add

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 function Auth(req, res, next) {
   let userToken = req.cookies.userToken;
   if (!userToken) {
-    res.status(401).send({ response: "page protected, sign in." });
+    res.redirect("/", 302);
   }
   let data = jwt.verify(userToken, process.env.api_secret);
   req.user = data;
